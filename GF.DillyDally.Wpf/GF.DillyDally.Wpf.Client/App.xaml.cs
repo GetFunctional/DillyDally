b187dc03da2b300f635e978164d0960690a239bf
getfunctional <mail@getfunctional.de>
2019-03-18 20:08:58 +0100

    MediatR & Lightinject

--- GF.DillyDally.Wpf/GF.DillyDally.Wpf.Client/App.xaml.cs
+++ GF.DillyDally.Wpf/GF.DillyDally.Wpf.Client/App.xaml.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using GF.DillyDally.Wpf.Client.Core;
 
 namespace GF.DillyDally.Wpf.Client
 {
     /// <summary>
-    /// Interaktionslogik für "App.xaml"
+    ///     Interaktionslogik für "App.xaml"
     /// </summary>
     public partial class App : Application
     {
+        private Bootstrapper _bootstrapper;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            this._bootstrapper = new Bootstrapper();
+            this._bootstrapper.Run();
+
+            var shell = new MainWindow();
+            shell.ShowDialog();
+        }
     }
 }
---
+++ GF.DillyDally.Wpf/GF.DillyDally.Wpf.Client/Core/Bootstrapper.cs
@@ -0,0 +1,36 @@
+﻿using LightInject;
+using MediatR;
+
+namespace GF.DillyDally.Wpf.Client.Core
+{
+    internal sealed class Bootstrapper
+    {
+        private readonly IServiceContainer _serviceContainer;
+
+        public Bootstrapper() : this(new ServiceContainer(new ContainerOptions
+            {EnablePropertyInjection = false, EnableVariance = false}))
+        {
+        }
+
+        private Bootstrapper(IServiceContainer serviceContainer)
+        {
+            this._serviceContainer = serviceContainer;
+        }
+
+        private void RegisterDependencies()
+        {
+        }
+
+        private void RegisterMediatRFramework(IServiceContainer serviceContainer)
+        {
+            serviceContainer.Register<IMediator, Mediator>();
+            serviceContainer.Register<ServiceFactory>(fac => fac.GetInstance);
+        }
+
+        public void Run()
+        {
+            this.RegisterMediatRFramework(this._serviceContainer);
+            this.RegisterDependencies();
+        }
+    }
+}
