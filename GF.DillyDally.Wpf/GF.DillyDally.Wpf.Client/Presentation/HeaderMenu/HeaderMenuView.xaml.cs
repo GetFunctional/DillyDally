bd321958434cad96514236d9c7ddc8201ada2338
Michael Morbach <michael.morbach@jtl-software.com>
2019-05-08 10:13:14 +0200

    Styling

---
+++ GF.DillyDally.Wpf/GF.DillyDally.Wpf.Client/Presentation/HeaderMenu/HeaderMenuView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GF.DillyDally.Wpf.Client.Presentation.HeaderMenu
+{
+    /// <summary>
+    ///     Interaktionslogik für HeaderMenuView.xaml
+    /// </summary>
+    public partial class HeaderMenuView : UserControl
+    {
+        public HeaderMenuView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
