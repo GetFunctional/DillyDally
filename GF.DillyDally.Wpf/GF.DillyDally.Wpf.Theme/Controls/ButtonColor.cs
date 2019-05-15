3f0b75077aa6d7a04f598939c18abc0650ec97f5
Michael Morbach <michael.morbach@jtl-software.com>
2019-05-15 10:24:04 +0200

    Colored Button Ready

---
+++ GF.DillyDally.Wpf/GF.DillyDally.Wpf.Theme/Controls/ButtonColor.cs
@@ -0,0 +1,13 @@
+﻿namespace GF.DillyDally.Wpf.Theme.Controls
+{
+    public enum ButtonColor
+    {
+        Gray = 0,
+        Blue = 1,
+        Green = 2,
+        Red = 3,
+        Black = 4,
+        Purple = 5,
+        Orange = 6
+    }
+}
--- GF.DillyDally.Wpf/GF.DillyDally.Wpf.Theme/Controls/ColoredButtonControl.cs
+++ GF.DillyDally.Wpf/GF.DillyDally.Wpf.Theme/Controls/ColoredButtonControl.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace GF.DillyDally.Wpf.Theme.Controls
@@ -6,5 +7,19 @@ namespace GF.DillyDally.Wpf.Theme.Controls
     [DesignTimeVisible(true)]
     public class ColoredButtonControl : Button
     {
+        public static readonly DependencyProperty ButtonColorProperty = DependencyProperty.Register(
+            "ButtonColor", typeof(ButtonColor), typeof(ColoredButtonControl), new PropertyMetadata(default(ButtonColor)));
+
+        public ButtonColor ButtonColor
+        {
+            get
+            {
+                return (ButtonColor)this.GetValue(ButtonColorProperty);
+            }
+            set
+            {
+                this.SetValue(ButtonColorProperty, value);
+            }
+        }
     }
 }
