752e43bb2ad7cab5804b247012f70b65d2a400f4
Michael Morbach <michael.morbach@jtl-software.com>
2019-05-28 16:55:43 +0200

    Styling Inputs

---
+++ GF.DillyDally.Wpf/GF.DillyDally.Wpf.Theme/Converter/IsNullToBooleanConverter.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GF.DillyDally.Wpf.Theme.Converter
+{
+    [ValueConversion(typeof(object), typeof(bool))]
+    public sealed class IsNullToBooleanConverter : IValueConverter
+    {
+        public bool IsInverted { get; set; }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (this.IsInverted)
+            {
+                return value != null;
+            }
+            return value == null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("No reverse conversion supported");
+        }
+
+        #endregion
+    }
+}
